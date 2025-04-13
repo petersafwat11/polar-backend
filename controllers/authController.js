cdea43894fb74da5cf30ffdc6bf75035d8f72b7e
peter safwat <petersafwat640@gmail.com>
2025-04-13 02:34:51 +0200

    updated the update password route

--- controllers/authController.js
+++ controllers/authController.js
@@ -241,16 +241,17 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
   // 1) Get user from collection
-  const user = await User.findById(req.user.id).select("+password");
+  const { id, currentPassword, password } = req.body;
+  const user = await User.findById(id).select("+password");
 
   // 2) Check if POSTed current password is correct
-  if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+  if (!(await user.correctPassword(currentPassword, user.password))) {
     return next(new AppError("Your current password is wrong.", 401));
   }
 
   // 3) If so, update password
-  user.password = req.body.password;
-  user.passwordConfirm = req.body.passwordConfirm;
+  user.password = password;
+  user.passwordConfirm = password;
   await user.save();
   // User.findByIdAndUpdate will NOT work as intended!
 
